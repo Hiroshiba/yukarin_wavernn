47b0447c798bea023047de27e69c29c906e2f7a0
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2021-01-20 04:16:37 +0000

    add cpp autoreg

---
+++ src_cython/check.py
@@ -0,0 +1,119 @@
+import numpy
+import torch
+import yukarin_autoreg_cpp
+from torch import Tensor
+from yukarin_wavernn.config import NetworkConfig
+from yukarin_wavernn.model import create_predictor
+from yukarin_wavernn.network.fast_forward import fast_generate, get_fast_forward_params
+
+max_batch_size = 4
+graph_length = 1000
+length = 4000
+config = NetworkConfig(
+    bit_size=10,
+    hidden_size=896,
+    local_size=5,
+    conditioning_size=80,
+    embedding_size=16,
+    linear_hidden_size=896,
+    local_scale=1,
+    local_layer_num=1,
+    speaker_size=0,
+    speaker_embedding_size=0,
+)
+model = create_predictor(config).eval()
+model.to("cuda")
+
+local_size = config.conditioning_size * 2 if config.conditioning_size is not None else 0
+
+base_x = torch.from_numpy(
+    numpy.random.randint(0, config.bit_size ** 2, size=(max_batch_size)).astype(
+        numpy.int32
+    )
+).to("cuda")
+base_l_array = torch.from_numpy(
+    numpy.random.rand(length, max_batch_size, local_size).astype(numpy.float32)
+).to("cuda")
+base_hidden = torch.from_numpy(
+    numpy.random.rand(max_batch_size, config.hidden_size).astype(numpy.float32)
+).to("cuda")
+
+params = get_fast_forward_params(model)
+
+
+def to_numpy(a):
+    if isinstance(a, Tensor):
+        a = a.detach().cpu().numpy()
+    return numpy.ascontiguousarray(a)
+
+
+# C++
+yukarin_autoreg_cpp.initialize(
+    graph_length=graph_length,
+    max_batch_size=max_batch_size,
+    local_size=local_size,
+    hidden_size=config.hidden_size,
+    embedding_size=config.embedding_size,
+    linear_hidden_size=config.linear_hidden_size,
+    output_size=2 ** config.bit_size,
+    x_embedder_W=to_numpy(params["x_embedder_W"]),
+    gru_xw=to_numpy(params["gru_xw"]),
+    gru_xb=to_numpy(params["gru_xb"]),
+    gru_hw=to_numpy(params["gru_hw"]),
+    gru_hb=to_numpy(params["gru_hb"]),
+    O1_W=to_numpy(params["O1_W"]),
+    O1_b=to_numpy(params["O1_b"]),
+    O2_W=to_numpy(params["O2_W"]),
+    O2_b=to_numpy(params["O2_b"]),
+)
+
+before_output = None
+for batch_size in [1, 2, 4]:
+    x = base_x[:batch_size].clone()
+    l_array = base_l_array[:, :batch_size].clone()
+    hidden = base_hidden[:batch_size].clone()
+
+    # x = model.xp.zeros_like(x)
+    # l_array = model.xp.zeros_like(l_array)
+    # hidden = model.xp.zeros_like(hidden)
+
+    output = numpy.ones((length, batch_size), dtype=numpy.int32) * -1
+    r = yukarin_autoreg_cpp.inference(
+        batch_size=batch_size,
+        length=length,
+        output=output,
+        x=to_numpy(x),
+        l_array=to_numpy(l_array),
+        hidden=to_numpy(hidden),
+    )
+    print(output)
+
+    if before_output is not None:
+        min_batch_size = min(before_output.shape[1], output.shape[1])
+        print(
+            "equal",
+            numpy.all(before_output[:, :min_batch_size] == output[:, :min_batch_size]),
+        )
+        assert numpy.all(
+            before_output[:, :min_batch_size] == output[:, :min_batch_size]
+        )
+    before_output = output
+
+
+with torch.no_grad():
+    expected = torch.stack(
+        fast_generate(
+            length=length,
+            x=base_x.clone().to(torch.int64),
+            l_array=torch.transpose(base_l_array.clone(), 0, 1),
+            h=base_hidden.clone(),
+            **params,
+        )
+    )
+    print("expected", expected)
+
+if output.shape == expected.shape:
+    print("equal", numpy.all(output == to_numpy(expected)))
+    assert numpy.all(output == to_numpy(expected))
+else:
+    print(expected)
--- tests/test_wavernn.py
+++ tests/test_wavernn.py
@@ -3,6 +3,10 @@ import unittest
 import torch
 import torch.nn.functional as F
 from parameterized import parameterized_class
+from yukarin_wavernn.network.fast_forward import (
+    fast_forward_one,
+    get_fast_forward_params,
+)
 from yukarin_wavernn.network.wave_rnn import WaveRNN
 
 batch_size = 2
@@ -16,6 +20,24 @@ speaker_size = 10
 def _make_hidden():
     hidden = torch.rand(batch_size, hidden_size, dtype=torch.float32)
     return hidden
+
+
+def get_fast_forward_params_one(wave_rnn):
+    fast_forward_params = get_fast_forward_params(wave_rnn)
+    dtype = fast_forward_params["gru_xb"].dtype
+    fast_forward_params["w_gru_x"] = torch.empty(
+        (batch_size, len(fast_forward_params["gru_xb"])), dtype=dtype
+    )
+    fast_forward_params["w_gru_h"] = torch.empty(
+        (batch_size, len(fast_forward_params["gru_hb"])), dtype=dtype
+    )
+    fast_forward_params["w_out_x1"] = torch.empty(
+        (batch_size, len(fast_forward_params["O1_b"])), dtype=dtype
+    )
+    fast_forward_params["w_out_x2"] = torch.empty(
+        (batch_size, len(fast_forward_params["O2_b"])), dtype=dtype
+    )
+    return fast_forward_params
 
 
 @parameterized_class(["with_speaker"], [[False], [True]])
@@ -56,7 +78,7 @@ class TestWaveRNN(unittest.TestCase):
             local_layer_num=2,
             speaker_size=speaker_size if with_speaker else 0,
             speaker_embedding_size=3 if with_speaker else 0,
-        )
+        ).eval()
 
         self.wave_rnn = wave_rnn
 
@@ -107,6 +129,24 @@ class TestWaveRNN(unittest.TestCase):
             hidden=hidden,
         )
 
+    def test_fast_forward_one(self):
+        wave_rnn = self.wave_rnn
+        hidden = _make_hidden()
+        s_one = (
+            self.s_one
+            if not wave_rnn.with_speaker
+            else wave_rnn.forward_speaker(self.s_one)
+        )
+        l_one = wave_rnn.forward_encode(l_array=self.l_one, s_one=s_one)
+        fast_forward_params = get_fast_forward_params_one(wave_rnn)
+        with torch.no_grad():
+            fast_forward_one(
+                prev_x=self.x_one[:, 0],
+                prev_l=l_one[:, 0],
+                hidden=hidden,
+                **fast_forward_params,
+            )
+
     def test_batchsize1_forward(self):
         wave_rnn = self.wave_rnn
         hidden = _make_hidden()
@@ -153,6 +193,35 @@ class TestWaveRNN(unittest.TestCase):
             l_one[:1, 0],
             hidden=hidden[:1],
         )
+
+        assert torch.allclose(oa[:1], ob, atol=1e-6)
+        assert torch.allclose(ha[:1], hb, atol=1e-6)
+
+    def test_batchsize1_fast_forward_one(self):
+        wave_rnn = self.wave_rnn
+        hidden = _make_hidden()
+        s_one = (
+            self.s_one
+            if not wave_rnn.with_speaker
+            else wave_rnn.forward_speaker(self.s_one)
+        )
+        l_one = wave_rnn.forward_encode(l_array=self.l_one, s_one=s_one)
+        fast_forward_params = get_fast_forward_params_one(wave_rnn)
+
+        with torch.no_grad():
+            oa, ha = fast_forward_one(
+                prev_x=self.x_one[:, 0],
+                prev_l=l_one[:, 0],
+                hidden=hidden,
+                **fast_forward_params,
+            )
+
+            ob, hb = fast_forward_one(
+                prev_x=self.x_one[:1, 0],
+                prev_l=l_one[:1, 0],
+                hidden=hidden[:1],
+                **fast_forward_params,
+            )
 
         assert torch.allclose(oa[:1], ob, atol=1e-6)
         assert torch.allclose(ha[:1], hb, atol=1e-6)
@@ -211,6 +280,42 @@ class TestWaveRNN(unittest.TestCase):
                 l[:, 0],
                 hb,
             )
+
+            assert torch.allclose(oa[:, :, i], ob, atol=1e-6)
+
+        assert torch.allclose(ha, hb, atol=1e-6)
+
+    def test_same_forward_rnn_and_fast_forward_one(self):
+        wave_rnn = self.wave_rnn
+        hidden = _make_hidden()
+        s_one = (
+            self.s_one
+            if not wave_rnn.with_speaker
+            else wave_rnn.forward_speaker(self.s_one)
+        )
+        l_array = wave_rnn.forward_encode(l_array=self.l_array, s_one=s_one)
+        fast_forward_params = get_fast_forward_params_one(wave_rnn)
+
+        oa, ha = wave_rnn.forward_rnn(
+            x_array=self.x_array,
+            l_array=l_array,
+            hidden=hidden,
+        )
+
+        hb = hidden
+        for i, (x, l) in enumerate(
+            zip(
+                torch.split(self.x_array, 1, dim=1),
+                torch.split(l_array, 1, dim=1),
+            )
+        ):
+            with torch.no_grad():
+                ob, hb = fast_forward_one(
+                    prev_x=x[:, 0],
+                    prev_l=l[:, 0],
+                    hidden=hb,
+                    **fast_forward_params,
+                )
 
             assert torch.allclose(oa[:, :, i], ob, atol=1e-6)
 
